63e94081c4d96c738d3370c03d4bf7ac2ce62328
Lukas Mateffy <luke@mateffy.me>
2021-05-20 02:32:37 +0200

    HSL support for BLE

--- src/modes/SolidColorMode.js
+++ src/modes/SolidColorMode.js
@@ -14,6 +14,35 @@ module.exports = class SolidColorMode extends RGBMode {
 		return this;
 	}
 
+	setHue(hue) {
+		this.color = this.chroma(...this.color, 'rgb')
+			.set('hsl.h', hue);
+
+		return this;
+	}
+
+	/**
+	 * Set color saturation
+	 * @param {number} saturation Saturation (0.0...1.0)
+	 */
+	setSaturation(saturation) {
+		this.color = this.chroma(...this.color, 'rgb')
+			.set('hsl.s', saturation);
+
+		return this;
+	}
+
+	/**
+	 * Set color brightness
+	 * @param {number} brightness Brightness (0.0...1.0)
+	 */
+	setBrightness(brightness) {
+		this.color = this.chroma(...this.color, 'rgb')
+			.set('hsl.l', brightness);
+
+		return this;
+	}
+
 	tick() {
 		// ignore tick, color stays
 		
