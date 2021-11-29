de0ee56dd4d5520ffaece6f65d77193d2eae2108
Lukas Mateffy <github@mateffy.me>
2021-11-29 23:31:34 +0100

    Examples

--- examples/custom-backend-led.js
+++ examples/custom-backend-led.js
@@ -5,55 +5,55 @@ const io = require('socket.io-client');
  * An LED backend that sends color values to a Socket.io server via WebSocket
  */
 class SocketIOLED extends BaseLED {
-	/**
-	 * Initialize a SocketIOLED
-	 * @param  {string} url The Socket.io server URL
-	 */
-	constructor(url) {
-		this.socket = io(url);
+    /**
+     * Initialize a SocketIOLED
+     * @param  {string} url The Socket.io server URL
+     */
+    constructor(url) {
+        this.socket = io(url);
 
-		this.socket
-			.on(
-				'connect', 
-				(socket) => console.log('Connected', socket.id)
-			);
+        this.socket
+            .on(
+                'connect', 
+                (socket) => console.log('Connected', socket.id)
+            );
 
-		this.socket
-			.on(
-				'disconnect', 
-				(socket) => console.log('Disconnected')
-			);
-	}
+        this.socket
+            .on(
+                'disconnect', 
+                (socket) => console.log('Disconnected')
+            );
+    }
 
-	/**
-	 * Called every tick if the color has changed.
-	 * RGB values are 0-255.
-	 * @abstract
-	 * @param {number} red
-	 * @param {number} green
-	 * @param {number} blue
-	 */
-	setRGB(red, green, blue) {
-		this.socket.send('rgb', { red, green, blue });
-	}
+    /**
+     * Called every tick if the color has changed.
+     * RGB values are 0-255.
+     * @abstract
+     * @param {number} red
+     * @param {number} green
+     * @param {number} blue
+     */
+    setRGB(red, green, blue) {
+        this.socket.send('rgb', { red, green, blue });
+    }
 
-	/**
-	 * Called when driver is shutdown.
-	 * Use this for cleanup!
-	 * @abstract
-	 */
-	destroy() {
-		this.socket.disconnect();
-	}
+    /**
+     * Called when driver is shutdown.
+     * Use this for cleanup!
+     * @abstract
+     */
+    destroy() {
+        this.socket.disconnect();
+    }
 }
 
 
 const rgb = new RGBLEDDriver({
-	// lower tick speed because we send values through the network anyway
-	tickSpeed: 15
+    // lower tick speed because we send values through the network anyway
+    tickSpeed: 15
 });
 
 rgb.setLED(new SocketIOLED('http://localhost:4000'));
 
 rgb.setMode('rainbow')
-	.setSpeed(1);
+    .setSpeed(1);
--- examples/debug-with-MockedLED.js
+++ examples/debug-with-MockedLED.js
@@ -3,41 +3,41 @@ const { RGBLEDDriver, MockedLED } = require('../');
 const chalk = require('chalk');
 
 function sleep(ms) {
-	return new Promise((resolve) => setTimeout(resolve, ms));
+    return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 async function main() {
-	const rgb = new RGBLEDDriver();
-	rgb.onTickError((e) => console.error('Hi!', e));
+    const rgb = new RGBLEDDriver();
+    rgb.onTickError((e) => console.error('Hi!', e));
 
-	// Mocked connect
-	rgb.setLED(new MockedLED(rgb));
+    // Mocked connect
+    rgb.setLED(new MockedLED(rgb));
 
-	while (true) {
-		rgb.setMode('solid', 0).setColor('#ff0000');
+    while (true) {
+        rgb.setMode('solid', 0).setColor('#ff0000');
 
-		await sleep(2000);
+        await sleep(2000);
 
-		rgb.setMode('solid').setColor('#00ffff');
+        rgb.setMode('solid').setColor('#00ffff');
 
-		rgb.setTransitionOverride([255, 0, 0], [0, 255, 255], 5000);
+        rgb.setTransitionOverride([255, 0, 0], [0, 255, 255], 5000);
 
-		await sleep(7000);
+        await sleep(7000);
 
-		rgb.setMode('notification', 1000)
-			.setDuration(1000)
-			.setTint(255, 0, 255, 'rgb');
+        rgb.setMode('notification', 1000)
+            .setDuration(1000)
+            .setTint(255, 0, 255, 'rgb');
 
-		await sleep(2000);
+        await sleep(2000);
 
-		rgb.setMode('rainbow').setSpeed(0.5);
+        rgb.setMode('rainbow').setSpeed(0.5);
 
-		await sleep(5000);
+        await sleep(5000);
 
-		rgb.setMode('random').setSpeed(2);
+        rgb.setMode('random').setSpeed(2);
 
-		await sleep(10_000);
-	}
+        await sleep(10_000);
+    }
 }
 
 main();
